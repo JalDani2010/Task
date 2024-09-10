ffe1052d2859f2c9e4839b9cf3a769ccdaf3fb3c
202201315 <202201315@daiict.ac.in>
2024-09-11 00:53:55 +0530

    push

--- app.js
+++ app.js
@@ -2,16 +2,27 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const tasks = require("./routes/tasks");
+const connectDB = require("./db/connect");
+require("dotenv").config();
 
 // middleware for logging and getting the data in req.body
 app.use(express.json());
 
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL);
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}....`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
 app.get("/hello", (req, res) => {
   res.send("Task manager app");
 });
 
-app.use("/api/v1/tasks", tasks); 
+app.use("/api/v1/tasks", tasks);
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}....`);
-});
+start();
